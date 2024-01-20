4640b4bf6407bbca971f4e33786066d184b08ed6
smurawczik <sebastian.murawczik@gmail.com>
2024-01-20 17:09:51 +0100

    fix dev command

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -7,9 +7,10 @@ export const meta: MetaFunction = () => [{ title: "Best place to work?" }];
 
 export default function Index() {
   const user = useOptionalUser();
+  console.log({ user });
+  
   return (
     <main className="relative min-h-screen bg-white sm:flex sm:items-center sm:justify-center">
-      chau
     </main>
   );
 }
