c051db18a3b4bd02a6fed72865f654b9a3f65430
smurawczik <sebastian.murawczik@gmail.com>
2024-01-23 23:20:32 +0100

    migrations test

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,10 +1,29 @@
 import { PrismaClient } from "@prisma/client";
 import bcrypt from "bcryptjs";
 
+import { geo_data } from "./seed.helpers/countries";
+
 const prisma = new PrismaClient();
 
 async function seed() {
   const email = "rachel@remix.run";
+
+  // Delete all data from the tables
+  await prisma.city.deleteMany().catch(() => {
+    // no worries if it doesn't exist yet
+  });
+
+  await prisma.country.deleteMany().catch(() => {
+    // no worries if it doesn't exist yet
+  });
+
+  await prisma.currency.deleteMany().catch(() => {
+    // no worries if it doesn't exist yet
+  });
+
+  await prisma.state.deleteMany().catch(() => {
+    // no worries if it doesn't exist yet
+  });
 
   // cleanup the existing database
   await prisma.user.deleteMany().catch(() => {
@@ -25,7 +44,7 @@ async function seed() {
     },
   });
 
-  const seba = await prisma.user.create({
+  await prisma.user.create({
     data: {
       email: "sebastian.murawczik@gmail.com",
       password: {
@@ -36,51 +55,64 @@ async function seed() {
     },
   });
 
-  await prisma.currency.create({
-    data: {
-      name: "USD",
-      code: "USD",
-    },
-  });
-
-  await prisma.salary.create({
-    data: {
-      amount: 10000,
-      currency: {
-        create: {
-          name: "EUR",
-          code: "EUR",
-        },
-      },
-      user: {
-        connect: {
-          id: seba.id,
-        },
-      },
-      category: "a",
-      city: {
-        create: {
-          name: "Berlin",
-          country: {
-            create: {
-              name: "Germany",
-            },
+  // Populate geographical data
+  await Promise.all(
+    geo_data.map(async (data) => {
+      await prisma.currency
+        .create({
+          data: {
+            name: data.currency_name,
+            code: data.currency,
+            symb: data.currency_symbol,
           },
+        })
+        .catch(() => {
+          console.log(`Currency ${data.currency} already exists.`);
+        });
+
+      const country = await prisma.country.create({
+        data: {
+          name: data.name,
+          emoji: data.emoji,
         },
-      },
-      remote: false,
-      country: {
-        create: {
-          name: "Germany",
-        },
-      },
-      company: {
-        create: {
-          name: "company",
-        },
-      },
-    },
-  });
+      });
+
+      await Promise.all(
+        data.states.map(async (state) => {
+          const createdState = await prisma.state.create({
+            data: {
+              name: state.name,
+              country: {
+                connect: {
+                  id: country.id,
+                },
+              },
+            },
+          });
+
+          await Promise.all(
+            state.cities.map(async (city) => {
+              await prisma.city.create({
+                data: {
+                  name: city.name,
+                  state: {
+                    connect: {
+                      id: createdState.id,
+                    },
+                  },
+                  country: {
+                    connect: {
+                      id: country.id,
+                    },
+                  },
+                },
+              });
+            })
+          );
+        })
+      );
+    })
+  );
 
   console.log(`Database has been seeded. 🌱`);
 }
